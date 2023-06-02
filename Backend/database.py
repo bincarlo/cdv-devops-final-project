107fbc838663388990cf05cf878098e4125cd33d
CDV-KK <kkesy1@edu.cdv.pl>
2023-06-02 10:19:31 +0200

    Change backend db connnection string to use dotenv variables
    * Load dotenv vars and use them in db connection URL
    * Add .env exmaple
    * Add python-dotenv to requirements.txt for pip install

--- Backend/database.py
+++ Backend/database.py
@@ -2,7 +2,16 @@ import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-DATABASE_URL = "postgresql://azureuser:postgres@localhost:5432/azureuser"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/postgres"
 
 engine = _sql.create_engine(DATABASE_URL)
 
