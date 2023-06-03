3b22858912637423e9bb6c8fce6e3af6a78488db
CDV-KK <kkesy1@edu.cdv.pl>
2023-06-03 12:16:45 +0200

    Add ansible playbook to custom data script

--- Infra/Terraform/modules/vm_scale_set/locals.tf
+++ Infra/Terraform/modules/vm_scale_set/locals.tf
@@ -24,56 +24,21 @@ echo "POSTGRES_USER=${var.postgres_user}" > /home/azureuser/cdv-devops-final-pro
 echo "POSTGRES_PASSWORD=${var.postgres_password}" >> /home/azureuser/cdv-devops-final-project/Backend/.env
 echo "POSTGRES_HOST=${var.postgres_host}" >> /home/azureuser/cdv-devops-final-project/Backend/.env
 
-# Install nginx
-sudo apt-get install nginx -y
-# Stop nginx
-sudo systemctl stop nginx
-# Enable nginx
-sudo systemctl enable nginx
+# # Install ansible
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
 
-# Update OpenSSL
-sudo NEEDRESTART_MODE=a apt-get upgrade openssl -y
+# # Run ansible setup playbook
+sudo ansible-playbook /home/azureuser/cdv-devops-final-project/Infra/Terraform/modules/vm_scale_set/setup.yaml
 
-# Install pip3
-sudo apt-get install python3-pip -y
-# Set python3.10 as default
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
-# Install NPM
-sudo apt-get install npm -y
-# Install NodeJS
-sudo apt-get install nodejs -y
-# install React
-sudo npm install -g create-react-app
+# # Go to repository
+cd /home/azureuser/cdv-devops-final-project/Frontend
+# # install requirements
+sudo npm install
 
-# Go to repository
-cd /home/azureuser/cdv-devops-final-project/Backend
-# Install requirements
-pip3 install -r requirements.txt
-# Resolve JWT error
-pip uninstall JWT -y
-pip uninstall PyJWT -y
-pip install PyJWT
-
-# Create python3 backend fastapi server
-sudo cp /home/azureuser/cdv-devops-final-project/Backend/fastapi.service /etc/systemd/system/fastapi.service
-# Enable python3 backend fastapi server
-sudo systemctl enable fastapi.service
-# Start python3 backend fastapi server
-sudo systemctl start fastapi.service
-
-# Go to repository
-cd ../Frontend
-# Install requirements
-npm install
-# Build react
-npm run build
-# Copy build to nginx
-sudo cp -r build/* /var/www/html/
-
-# Copy nginx config
-sudo cp ../Infra/Legacy/nginx.conf /etc/nginx/sites-available/default
-# Start nginx
-sudo systemctl start nginx
+# # Run ansible setup playbook - part 2
+sudo ansible-playbook /home/azureuser/cdv-devops-final-project/Infra/Terraform/modules/vm_scale_set/setup-p2.yaml
 
 # Log custom_data finish time
 date >> /home/azureuser/custom-data.log
